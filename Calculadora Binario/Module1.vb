6f1ce3a7a6862cec61317aa53ecc72fa9e648675
vistaero <vistaero22@hotmail.com>
2013-10-01 11:11:56 +0200

    Optimización del código
    Se realizan todas las comprobaciones por las que el programa podría
    fallar en vez de usar un try.

--- Calculadora Binario/Module1.vb
+++ Calculadora Binario/Module1.vb
@@ -12,28 +12,40 @@
         Dim bits As Integer = 0
         Dim cifra As Long = 0
         reply = Console.ReadLine()
-        Try
-            cifra = reply
-        Catch ex As Exception
-            Console.WriteLine("Respuesta no válida, revise su número e inténtelo de nuevo: " & ex.Message)
-            BinarioADecimal()
 
-        End Try
-
-        For Each digit In cifra.ToString
-            If digit.ToString > 1 Then
-                Console.WriteLine("No es un número binario")
+        ' Realizar comprobaciones por las que podría fallar para evitar usar try
+        Dim sonnumeros As Boolean = False
+        Dim numerosmayoresqueuno As Boolean = False
+        ' Primero comprobamos si la respuesta es demasiado larga
+        If reply.LongCount < 20 Then
+            For Each digit As Char In reply
+                ' Comprobar si todos los caracteres de la respuesta son números
+                sonnumeros = Char.IsDigit(digit)
+                ' Comprobar que ningún número sea mayor que 1
+                If sonnumeros = True Then
+                    If digit.ToString > 1 Then
+                        numerosmayoresqueuno = True
+                    End If
+                End If
+            Next
+            ' Comprobamos si se cumple alguna de las condiciones no permitidas
+            If sonnumeros.Equals(False) OrElse numerosmayoresqueuno = True Then
+                Console.WriteLine("Un número binario sólo puede tener 0 y/o 1.")
                 BinarioADecimal()
-
             End If
-        Next
+        Else
+            Console.WriteLine("Este programa tiene un límite técnico de 19 cifras por número.")
+            BinarioADecimal()
+        End If
+        
+        ' Se han superado todas las comprobaciones
 
         bits = reply.Length
         Select Case bits
             Case Is > 1
-                Console.WriteLine(cifra & " es de " & bits & " bits.")
+                Console.WriteLine(reply & " es de " & bits & " bits.")
             Case Is < 2
-                Console.WriteLine(cifra & " es de " & bits & " bit.")
+                Console.WriteLine(reply & " es de " & bits & " bit.")
         End Select
 
         'Comienza la conversión
@@ -41,7 +53,12 @@
         Dim resultado As Long = 0
         Dim digitinteger As Integer = 0
 
-        For Each digit In cifra.ToString
+        For Each digit In reply
+
+            ' Mi fascinante motor de conversión que me llevó toda la santa tarde.
+            ' Y yo suspendía matemáticas...
+            ' Mi ego va a estar por las nubes durante lustros por "inventar" esta ecuación.
+            ' Sin buscar en Google ni preguntar a nadie, ¡¿quién es el hombre?!
 
             digitinteger = digit.ToString
             Dim valor As Integer = 0
